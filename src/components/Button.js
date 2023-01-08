7ad42227efb9ea911da9b1dfa0c35790e10deb03
Moon Kabir <moonkabir4@gmail.com>
2023-01-08 23:38:00 +0600

    Quiz page done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,4 @@
 import Classes from "../styles/Button.module.css";
-export default function Button({ children }) {
-  return (
-    <div className={Classes.button}>
-      <span>{children}</span>
-    </div>
-  );
+export default function Button({ className, children }) {
+  return <div className={`${Classes.button} ${className}`}>{children}</div>;
 }
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -25,7 +25,9 @@ export default function Signup() {
             icon="lock_clock"
           />
           <Checkbox text="I agree to the Terms & Conditions" />
-          <Button>Submit now</Button>
+          <Button>
+            <span>Submit now</span>
+          </Button>
           <div className="info">
             Already have an account? <a href="login.html">Login</a> instead.
           </div>
