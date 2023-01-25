b1a413648d4d59cc6e6edc509cbf872dd5405f03
Moon Kabir <moonkabir4@gmail.com>
2023-01-25 22:17:21 +0600

    Routing manage and video a tag issue solve video 38 part 11

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,15 +2,13 @@ import image from "../assets/images/3.jpg";
 import Classes from "../styles/Video.module.css";
 export default function Videos() {
   return (
-    <a href="quiz.html">
-      <div className={Classes.video}>
-        <img src={image} alt="Video Title" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-        <div className={Classes.qmeta}>
-          <p>10 Questions</p>
-          <p>Score : Not taken yet</p>
-        </div>
+    <div className={Classes.video}>
+      <img src={image} alt="Video Title" />
+      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+      <div className={Classes.qmeta}>
+        <p>10 Questions</p>
+        <p>Score : Not taken yet</p>
       </div>
-    </a>
+    </div>
   );
 }
